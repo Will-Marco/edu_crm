d298debd19e7096e0d24f49ae3b3310f3e993607
Will Marco <mr.marco0101@gmail.com>
2021-11-22 00:09:51 +0500

    Add aplicant route

--- src/helpers/permissionChecker.js
+++ src/helpers/permissionChecker.js
@@ -3,10 +3,13 @@ module.exports = function PermissionChecker(
   permissions,
   error
 ) {
-  console.log(permissions);
-  let permission = permissions.find(
-    (x) => x["permission.permission_name"] == permission_name
-  );
+  let permission = permissions.find((x) => {
+    if (Array.isArray(permission_name)) {
+      return permission_name.includes(x["permission.permission_name"]);
+    } else {
+      return x["permission.permission_name"] == permission_name;
+    }
+  });
 
   if (!permission) throw new error(401, "You haven't permission");
 };
--- src/modules/pg/postgres.js
+++ src/modules/pg/postgres.js
@@ -7,6 +7,7 @@ const relations = require("./relations");
 const UserPermissionModel = require("../../models/UserPermissionModel");
 const TeachersModel = require("../../models/TeachersModel");
 const CourseModel = require("../../models/CourseModel");
+const ApplicantModel = require("../../models/ApplicantModel");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
@@ -21,6 +22,7 @@ module.exports = async function postgres() {
     db.user_permissions = await UserPermissionModel(sequelize, Sequelize);
     db.teachers = await TeachersModel(sequelize, Sequelize);
     db.courses = await CourseModel(sequelize, Sequelize);
+    db.applicants = await ApplicantModel(sequelize, Sequelize);
 
     await relations(db);
 
